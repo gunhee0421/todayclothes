e4670b7a013b065d964f7286452f47af603a71ad
seowooda <koeun0409@gmail.com>
2024-10-31 23:54:02 +0900

    design: 로그인 페이지 수정

--- src/app/(before)/login/page.tsx
+++ src/app/(before)/login/page.tsx
@@ -26,8 +26,8 @@ const Login = () => {
         }
 
   return (
-    <div className="flex min-h-screen w-full max-w-[37.5rem] flex-col bg-gray-100 px-[2.25rem] py-[6rem]">
-      <div className="flex w-full flex-col gap-[13rem] sm:gap-[15rem]">
+    <div className="flex min-h-screen w-full max-w-[37.5rem] flex-col items-center justify-center bg-gray-100 px-[2.25rem]">
+      <div className="flex w-full flex-col gap-[13rem] sm:gap-[22rem]">
         <div className="flex w-full animate-fadein flex-col gap-[1rem] py-[2rem]">
           <p className="font-notosanko text-[1.3rem] font-bold sm:text-[1.75rem]">
             {textContent.welcome.split('\n').map((text, index) => (
@@ -52,7 +52,7 @@ const Login = () => {
           >
             <button className="flex w-full items-center justify-between rounded-[0.25rem] bg-white px-[1rem] py-[0.625rem] hover:bg-slate-50">
               <img src="/icon/google.svg" alt="google" width="24" height="24" />
-              <p className="flex-1 text-center font-notosanko text-[0.75rem] font-medium">
+              <p className="flex-1 font-notosanko text-[0.75rem] font-medium">
                 {textContent.google}
               </p>
             </button>
@@ -67,7 +67,7 @@ const Login = () => {
           >
             <button className="flex w-full items-center rounded-[0.25rem] bg-yellow-300 px-[1rem] py-[0.625rem] hover:bg-yellow-200">
               <img src="/icon/kakao.svg" alt="kakao" width="24" height="24" />
-              <p className="flex-1 text-center font-notosanko text-[0.75rem] font-medium">
+              <p className="flex-1 font-notosanko text-[0.75rem] font-medium">
                 {textContent.kakao}
               </p>
             </button>

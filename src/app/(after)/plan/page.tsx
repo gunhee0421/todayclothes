2c0bc685e60576f9f34310632cf7c680f192490f
getsoss <getsoss@naver.com>
2024-11-06 15:27:21 +0900

    Fix/배경색 변경

--- src/app/(after)/plan/page.tsx
+++ src/app/(after)/plan/page.tsx
@@ -170,252 +170,254 @@ const Plan: React.FC = () => {
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white">
-      <div className="w-[600px] rounded-[16px] p-[32px]">
-        <h2 className="mb-[36px] text-center font-notosanko text-[20px] font-semibold leading-normal tracking-[-0.1px]">
-          {language === 'ko'
-            ? '오늘의 주요 일정을 입력해주세요.'
-            : 'What are your main plans for today?'}
-        </h2>
+    <body className="bg-white">
+      <div className="flex min-h-screen items-center justify-center bg-white">
+        <div className="w-[600px] rounded-[16px] p-[32px]">
+          <h2 className="mb-[36px] text-center font-notosanko text-[20px] font-semibold leading-normal tracking-[-0.1px]">
+            {language === 'ko'
+              ? '오늘의 주요 일정을 입력해주세요.'
+              : 'What are your main plans for today?'}
+          </h2>
 
-        <form onSubmit={handleSubmit(handleLog)}>
-          {/* Activity Location */}
-          <div>
-            <label className="font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
-              {language === 'ko' ? '활동 장소' : 'Activity Location'}
+          <form onSubmit={handleSubmit(handleLog)}>
+            {/* Activity Location */}
+            <div>
+              <label className="font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
+                {language === 'ko' ? '활동 장소' : 'Activity Location'}
+              </label>
+              <div className="w-[536px] pb-[40px] pt-[8px]">
+                <Controller
+                  name="selectedPlace"
+                  control={control}
+                  render={({ field }) => (
+                    <GooglePlacesAutocomplete
+                      selectProps={{
+                        value: field.value,
+                        onChange: handlePlaceChange,
+                        placeholder:
+                          language === 'ko'
+                            ? '활동 장소를 선택해주세요.'
+                            : 'Enter activity location.',
+                        styles: {
+                          control: (provided) => ({
+                            ...provided,
+                            height: '3rem',
+                            backgroundColor: 'rgb(241 241 244)',
+                            border: '0px solid',
+                            borderRadius: '8px',
+                            color: '#3C4350',
+                          }),
+                          input: (provided) => ({
+                            ...provided,
+                            color: '#3C4350',
+                          }),
+                        },
+                      }}
+                      apiKey={process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}
+                    />
+                  )}
+                />
+              </div>
+            </div>
+
+            {/* Activity Time */}
+            <label className="w-full font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
+              {language === 'ko' ? '활동 시간' : 'Activity Time'}
             </label>
-            <div className="w-[536px] pb-[40px] pt-[8px]">
+            <div className="flex pb-[40px] pt-[8px]">
               <Controller
-                name="selectedPlace"
+                name="startTime"
                 control={control}
                 render={({ field }) => (
-                  <GooglePlacesAutocomplete
-                    selectProps={{
-                      value: field.value,
-                      onChange: handlePlaceChange,
-                      placeholder:
-                        language === 'ko'
-                          ? '활동 장소를 선택해주세요.'
-                          : 'Enter activity location.',
-                      styles: {
-                        control: (provided) => ({
-                          ...provided,
-                          height: '3rem',
-                          backgroundColor: 'rgb(241 241 244)',
-                          border: '0px solid',
-                          borderRadius: '8px',
-                          color: '#3C4350',
-                        }),
-                        input: (provided) => ({
-                          ...provided,
-                          color: '#3C4350',
-                        }),
-                      },
-                    }}
-                    apiKey={process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}
-                  />
+                  <LocalizationProvider
+                    dateAdapter={AdapterDayjs}
+                    adapterLocale={language === 'ko' ? 'ko' : 'en'} // Set locale dynamically
+                  >
+                    <DatePicker
+                      {...field}
+                      value={field.value || dayjs()} // Default to today's date if not set
+                      disablePast
+                      shouldDisableDate={(date) => !isDateSelectable(date)}
+                      sx={{
+                        backgroundColor: 'rgb(241, 241, 244)',
+                        border: 'none',
+                        borderRadius: '8px',
+                        width: '100%',
+                      }}
+                    />
+                  </LocalizationProvider>
                 )}
               />
             </div>
-          </div>
 
-          {/* Activity Time */}
-          <label className="w-full font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
-            {language === 'ko' ? '활동 시간' : 'Activity Time'}
-          </label>
-          <div className="flex pb-[40px] pt-[8px]">
-            <Controller
-              name="startTime"
-              control={control}
-              render={({ field }) => (
-                <LocalizationProvider
-                  dateAdapter={AdapterDayjs}
-                  adapterLocale={language === 'ko' ? 'ko' : 'en'} // Set locale dynamically
-                >
-                  <DatePicker
-                    {...field}
-                    value={field.value || dayjs()} // Default to today's date if not set
-                    disablePast
-                    shouldDisableDate={(date) => !isDateSelectable(date)}
-                    sx={{
-                      backgroundColor: 'rgb(241, 241, 244)',
-                      border: 'none',
-                      borderRadius: '8px',
-                      width: '100%',
-                    }}
-                  />
-                </LocalizationProvider>
-              )}
-            />
-          </div>
+            {/* Time of Day */}
+            <div>
+              <label className="font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
+                {language === 'ko' ? '활동 시간대' : 'Time of Day'}
+              </label>
+              <Controller
+                name="timeOfDay"
+                control={control}
+                render={({ field }) => (
+                  <div className="grid grid-cols-2 gap-[8px] pb-[40px] pt-[8px]">
+                    {[
+                      TimeOfDay.Morning,
+                      TimeOfDay.Afternoon,
+                      TimeOfDay.Evening,
+                      TimeOfDay.Night,
+                    ].map((time) => (
+                      <button
+                        key={time}
+                        type="button"
+                        onClick={() => field.onChange(time)}
+                        className={`rounded-[16px] px-2 py-4 font-notosanko font-medium ${
+                          field.value === time
+                            ? 'bg-red-100 text-red-600'
+                            : 'bg-gray-100 text-gray-600'
+                        }`}
+                      >
+                        {language === 'ko'
+                          ? time === TimeOfDay.Morning
+                            ? '아침'
+                            : time === TimeOfDay.Afternoon
+                              ? '낮'
+                              : time === TimeOfDay.Evening
+                                ? '저녁'
+                                : '밤'
+                          : time}
+                      </button>
+                    ))}
+                  </div>
+                )}
+              />
+            </div>
 
-          {/* Time of Day */}
-          <div>
-            <label className="font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
-              {language === 'ko' ? '활동 시간대' : 'Time of Day'}
-            </label>
-            <Controller
-              name="timeOfDay"
-              control={control}
-              render={({ field }) => (
-                <div className="grid grid-cols-2 gap-[8px] pb-[40px] pt-[8px]">
-                  {[
-                    TimeOfDay.Morning,
-                    TimeOfDay.Afternoon,
-                    TimeOfDay.Evening,
-                    TimeOfDay.Night,
-                  ].map((time) => (
+            <div>
+              <label className="font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
+                {language === 'ko' ? '성별' : 'Gender'}
+              </label>
+              <Controller
+                name="gender"
+                control={control}
+                render={({ field }) => (
+                  <div className="flex gap-[16px] pb-[40px] pt-[8px]">
                     <button
-                      key={time}
                       type="button"
-                      onClick={() => field.onChange(time)}
-                      className={`rounded-[16px] px-2 py-4 font-notosanko font-medium ${
-                        field.value === time
+                      onClick={() => field.onChange(Gender.Female)}
+                      className={`flex-1 rounded-[16px] px-2 py-4 font-notosanko font-medium ${
+                        field.value === Gender.Female
                           ? 'bg-red-100 text-red-600'
                           : 'bg-gray-100 text-gray-600'
                       }`}
                     >
-                      {language === 'ko'
-                        ? time === TimeOfDay.Morning
-                          ? '아침'
-                          : time === TimeOfDay.Afternoon
-                            ? '낮'
-                            : time === TimeOfDay.Evening
-                              ? '저녁'
-                              : '밤'
-                        : time}
+                      {language === 'ko' ? '여성' : 'Female'}
                     </button>
-                  ))}
-                </div>
-              )}
-            />
-          </div>
-
-          <div>
-            <label className="font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
-              {language === 'ko' ? '성별' : 'Gender'}
-            </label>
-            <Controller
-              name="gender"
-              control={control}
-              render={({ field }) => (
-                <div className="flex gap-[16px] pb-[40px] pt-[8px]">
-                  <button
-                    type="button"
-                    onClick={() => field.onChange(Gender.Female)}
-                    className={`flex-1 rounded-[16px] px-2 py-4 font-notosanko font-medium ${
-                      field.value === Gender.Female
-                        ? 'bg-red-100 text-red-600'
-                        : 'bg-gray-100 text-gray-600'
-                    }`}
-                  >
-                    {language === 'ko' ? '여성' : 'Female'}
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => field.onChange(Gender.Male)}
-                    className={`flex-1 rounded-[16px] px-2 py-4 font-notosanko font-medium ${
-                      field.value === Gender.Male
-                        ? 'bg-red-100 text-red-600'
-                        : 'bg-gray-100 text-gray-600'
-                    }`}
-                  >
-                    {language === 'ko' ? '남성' : 'Male'}
-                  </button>
-                </div>
-              )}
-            />
-          </div>
-
-          <div>
-            <label className="font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
-              {language === 'ko' ? '활동 종류' : 'Activity Type'}
-            </label>
-            <Controller
-              name="activityType"
-              control={control}
-              render={({ field }) => (
-                <div className="flex gap-[16px] pb-[40px] pt-[8px]">
-                  <button
-                    type="button"
-                    onClick={() => field.onChange(ActivityType.Indoor)}
-                    className={`flex-1 rounded-[16px] px-2 py-4 font-notosanko font-medium ${
-                      field.value === ActivityType.Indoor
-                        ? 'bg-red-100 text-red-600'
-                        : 'bg-gray-100 text-gray-600'
-                    }`}
-                  >
-                    {language === 'ko' ? '실내' : 'Indoors'}
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => field.onChange(ActivityType.Outdoor)}
-                    className={`flex-1 rounded-[16px] px-2 py-4 font-notosanko font-medium ${
-                      field.value === ActivityType.Outdoor
-                        ? 'bg-red-100 text-red-600'
-                        : 'bg-gray-100 text-gray-600'
-                    }`}
-                  >
-                    {language === 'ko' ? '실외' : 'Outdoors'}
-                  </button>
-                </div>
-              )}
-            />
-          </div>
-
-          <div>
-            <label className="font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
-              {language === 'ko' ? '활동 분위기' : 'Activity Style'}
-            </label>
-            <Controller
-              name="activityStyle"
-              control={control}
-              render={({ field }) => (
-                <div className="mb-[36px] grid grid-cols-2 gap-[8px] gap-x-[16px] pt-[8px]">
-                  {activityStyles.map((style) => (
                     <button
-                      key={style}
                       type="button"
-                      onClick={() => field.onChange(style)}
-                      className={`rounded-[16px] px-2 py-4 font-notosanko font-medium ${
-                        field.value === style
+                      onClick={() => field.onChange(Gender.Male)}
+                      className={`flex-1 rounded-[16px] px-2 py-4 font-notosanko font-medium ${
+                        field.value === Gender.Male
                           ? 'bg-red-100 text-red-600'
                           : 'bg-gray-100 text-gray-600'
                       }`}
                     >
-                      {translateActivityStyle(style, language)}
+                      {language === 'ko' ? '남성' : 'Male'}
                     </button>
-                  ))}
-                </div>
-              )}
-            />
-          </div>
+                  </div>
+                )}
+              />
+            </div>
 
-          {/* Buttons */}
-          <div className="flex gap-[12px]">
-            <button
-              type="button"
-              onClick={() => router.push('/home')}
-              className="flex-1 rounded-[8px] bg-red-100 py-2 font-notosanko text-red-600"
-            >
-              {language === 'ko' ? '취소' : 'Cancel'}
-            </button>
-            <button
-              type="submit"
-              className={`flex-1 rounded-[8px] py-2 font-notosanko text-white ${
-                !isFormValid ? 'bg-gray-300' : 'bg-red-500'
-              }`}
-              disabled={!isFormValid}
-            >
-              {language === 'ko' ? '생성' : 'Generate'}
-            </button>
-          </div>
-        </form>
+            <div>
+              <label className="font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
+                {language === 'ko' ? '활동 종류' : 'Activity Type'}
+              </label>
+              <Controller
+                name="activityType"
+                control={control}
+                render={({ field }) => (
+                  <div className="flex gap-[16px] pb-[40px] pt-[8px]">
+                    <button
+                      type="button"
+                      onClick={() => field.onChange(ActivityType.Indoor)}
+                      className={`flex-1 rounded-[16px] px-2 py-4 font-notosanko font-medium ${
+                        field.value === ActivityType.Indoor
+                          ? 'bg-red-100 text-red-600'
+                          : 'bg-gray-100 text-gray-600'
+                      }`}
+                    >
+                      {language === 'ko' ? '실내' : 'Indoors'}
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => field.onChange(ActivityType.Outdoor)}
+                      className={`flex-1 rounded-[16px] px-2 py-4 font-notosanko font-medium ${
+                        field.value === ActivityType.Outdoor
+                          ? 'bg-red-100 text-red-600'
+                          : 'bg-gray-100 text-gray-600'
+                      }`}
+                    >
+                      {language === 'ko' ? '실외' : 'Outdoors'}
+                    </button>
+                  </div>
+                )}
+              />
+            </div>
 
-        {/* Toast container for notifications */}
-        <ToastContainer />
+            <div>
+              <label className="font-notosanko text-[12px] font-medium leading-normal text-zinc-400">
+                {language === 'ko' ? '활동 분위기' : 'Activity Style'}
+              </label>
+              <Controller
+                name="activityStyle"
+                control={control}
+                render={({ field }) => (
+                  <div className="mb-[36px] grid grid-cols-2 gap-[8px] gap-x-[16px] pt-[8px]">
+                    {activityStyles.map((style) => (
+                      <button
+                        key={style}
+                        type="button"
+                        onClick={() => field.onChange(style)}
+                        className={`rounded-[16px] px-2 py-4 font-notosanko font-medium ${
+                          field.value === style
+                            ? 'bg-red-100 text-red-600'
+                            : 'bg-gray-100 text-gray-600'
+                        }`}
+                      >
+                        {translateActivityStyle(style, language)}
+                      </button>
+                    ))}
+                  </div>
+                )}
+              />
+            </div>
+
+            {/* Buttons */}
+            <div className="flex gap-[12px]">
+              <button
+                type="button"
+                onClick={() => router.push('/home')}
+                className="flex-1 rounded-[8px] bg-red-100 py-2 font-notosanko text-red-600"
+              >
+                {language === 'ko' ? '취소' : 'Cancel'}
+              </button>
+              <button
+                type="submit"
+                className={`flex-1 rounded-[8px] py-2 font-notosanko text-white ${
+                  !isFormValid ? 'bg-gray-300' : 'bg-red-500'
+                }`}
+                disabled={!isFormValid}
+              >
+                {language === 'ko' ? '생성' : 'Generate'}
+              </button>
+            </div>
+          </form>
+
+          {/* Toast container for notifications */}
+          <ToastContainer />
+        </div>
       </div>
-    </div>
+    </body>
   )
 }
 
